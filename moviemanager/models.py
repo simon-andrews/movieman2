b54a6353a746d54869a7cadca1bdcfb1e1cd3d51
Simon Andrews <simon@simonandrews.org>
2015-04-29 17:36:33 -0400

    Add some extra fields to movie model

--- moviemanager/models.py
+++ moviemanager/models.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Movie(models.Model):
     tmdb_id = models.IntegerField()
+    score = models.IntegerField()
+    submitter = models.ForeignKey(User)
