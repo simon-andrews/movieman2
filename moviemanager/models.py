b448ce0d1333c8feb13f8dccae55885d4a1855ff
Simon Andrews <simon@simonandrews.org>
2015-04-30 19:00:13 -0400

    Store movie metadata on server side

--- moviemanager/models.py
+++ moviemanager/models.py
@@ -3,6 +3,17 @@ from django.db import models
 
 
 class Movie(models.Model):
+    # General
     tmdb_id = models.IntegerField()
     score = models.IntegerField()
     submitter = models.ForeignKey(User)
+
+    # Movie metadata
+    title = models.CharField(max_length=128)
+    tagline = models.TextField()
+    overview = models.TextField()
+    release_date = models.DateField()
+    budget = models.IntegerField()
+    vote_average = models.DecimalField(max_digits=2, decimal_places=1)
+    vote_count = models.IntegerField()
+    original_language = models.CharField(max_length=2)
