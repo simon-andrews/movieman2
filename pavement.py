b36eb09bb85bb4eee0db9669745e0c1adc244980
Simon Andrews <simon@simonandrews.org>
2015-04-30 19:31:21 -0400

    Write default config with paver.

---
+++ pavement.py
@@ -0,0 +1,23 @@
+from paver.easy import *
+
+
+config = """# replace pass with values you would like to overwrite from DefaultConfig in
+# default_config.py. Values you do not explicitly overwrite will be inherited
+# from DefaultConfig. At the very least, you must set secret_key and
+# tmdb_api_key.
+
+from default_config import DefaultConfig
+
+
+class Config(DefaultConfig):
+    pass
+"""
+
+
+@task
+def setup():
+    """Writes a default config to config.py"""
+    print('Writing default config.')
+    f = open('config.py', 'w')
+    f.write(config)
+    f.close()
