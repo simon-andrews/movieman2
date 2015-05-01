27a680519e7db5c9c48c951235400e0c814f8562
Simon Andrews <simon@simonandrews.org>
2015-04-30 21:31:59 -0400

    Customize movie admin interface

--- moviemanager/admin.py
+++ moviemanager/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Movie
 
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'score',
+        'submitter',
+        'release_date',
+        'tmdb_id'
+    )
+
 # Register your models here.
-admin.site.register(Movie)
+admin.site.register(Movie, MovieAdmin)
