91adbe8f8cb69f774ed28141bda0fe85cc802c22
Simon Andrews <simon@simonandrews.org>
2015-05-05 19:18:55 -0400

    Censor adult film covers

--- moviemanager/views.py
+++ moviemanager/views.py
@@ -4,7 +4,7 @@ import tmdbsimple as tmdb
 
 def add_movies(request, movie_title):
     search = tmdb.Search()
-    search.movie(query=movie_title)
+    search.movie(query=movie_title, include_adult=True)
     dictionary = {
         "movies": search.results
     }
