32a94dc2b7820dd2c498e154a3f5f3c0e84949d3
Simon Andrews <simon@simonandrews.org>
2015-05-07 17:01:24 -0400

    A custom secret key is not *required* to run.

--- default_config.py
+++ default_config.py
@@ -1,3 +1,3 @@
 class DefaultConfig():
-    secret_key = None  # Django secret key used for sessions. Make this a long random string and keep it a secret
+    secret_key = "CHANGEME"  # Django secret key used for sessions. Make this a long random string and keep it a secret
     tmdb_api_key = None  # API key for The Movie Database (themoviedb.org).
