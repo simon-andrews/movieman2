e78b8704a68b49593b43ac5b9690be36fca2e274
Simon Andrews <simon@simonandrews.org>
2015-05-07 16:42:04 -0400

    Set default config values to none so user is forced to set them.
    Also document fields in the default config

--- default_config.py
+++ default_config.py
@@ -1,3 +1,3 @@
 class DefaultConfig():
-    secret_key = "LONG_AND_RANDOM"
-    tmdb_api_key = "THEMOVIEDB.ORG_API_KEY"
+    secret_key = None  # Django secret key used for sessions. Make this a long random string and keep it a secret
+    tmdb_api_key = None  # API key for The Movie Database (themoviedb.org).
