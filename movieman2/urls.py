d7b0d51bea590459e5817576c32d0e74d7d583ce
Simon Andrews <simon@simonandrews.org>
2015-05-12 17:45:51 -0400

    Add new page for showing a single movie

--- movieman2/urls.py
+++ movieman2/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns(
     '',
     url(r'^add_movies/(?P<movie_title>.*)$', views.add_movies, name="add_movies"),
     url(r'^list_saved_movies$', views.list_saved_movies, name="list_saved_movies"),
+    url(r'^show_movie/(?P<movie_id>\d*)', views.show_movie, name="show_movie"),
 )
--- movieman2/views.py
+++ movieman2/views.py
@@ -19,3 +19,11 @@ def list_saved_movies(request):
         "movies": Movie.objects.all()
     }
     return render_to_response("moviemanager/list_saved_movies.html", dictionary=dictionary)
+
+
+def show_movie(request, movie_id):
+    movie_data = tmdb.Movies(movie_id).info()
+    dictionary = {
+        "movie": movie_data,
+    }
+    return render_to_response("moviemanager/movie.html", dictionary=dictionary)
