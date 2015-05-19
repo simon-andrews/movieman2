c5d364fc168d5faedd9c7a99f330e44ece5ee229
Math-ias <m.kools@comcast.net>
2015-05-18 20:26:10 -0400

    Correctly load key from environment
    This closes an issue of if a key was not exported in an environment
    it would raise a KeyError exception, the method now used resolves that
    issue as it returns None instead of an error if the key does not exist,
    allowing it to revert to the key set in the settings.

--- movieman2/__init__.py
+++ movieman2/__init__.py
@@ -2,4 +2,4 @@ import os
 import tmdbsimple
 from django.conf import settings
 
-tmdbsimple.API_KEY = os.environ['MM2_TMDB_API_KEY'] or settings.MM2_TMDB_API_KEY
+tmdbsimple.API_KEY = os.environ.get('MM2_TMDB_API_KEY') or settings.MM2_TMDB_API_KEY
