80200f094c06b25b7a0a607ab84f0570c48022c5
Simon Andrews <simon@simonandrews.org>
2015-05-18 21:29:27 -0400

    Add package classifiers and author email to setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='movieman2',
-    version="0.2",
+    version='0.2',
     packages=['movieman2'],
     include_package_data=True,
     license='MIT License',
@@ -18,6 +18,22 @@ setup(
     long_description=README,
     url='https://github.com/simon-andrews/movieman2',
     author='Simon Andrews',
+    author_email='http://scr.im/35r3',
     install_requires=['Django', 'tmdbsimple'],
-    test_suite='run_tests'
+    test_suite='run_tests',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ]
 )
